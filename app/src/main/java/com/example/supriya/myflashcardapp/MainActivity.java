1816bb0bbde166d689b86d29708ab7c9c7907a12
spalli235 <49597499+spalli235@users.noreply.github.com>
2020-02-20 13:44:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/supriya/myflashcardapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.supriya.myflashcardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
