7a67a0c3d60fc101a26245358a494352b3e0fda4
spalli235 <49597499+spalli235@users.noreply.github.com>
2020-03-07 01:16:07 -0500

    Completed required tasks for Lab 4 & ready for submission

--- app/src/main/java/com/example/supriya/myflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/supriya/myflashcardapp/AddCardActivity.java
@@ -1,7 +1,10 @@
 package com.example.supriya.myflashcardapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.my_cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.my_save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String storeQuestion = ((EditText) findViewById(R.id.editQuestionField)).getText().toString();
+                String storeAnswer = ((EditText) findViewById(R.id.editAnswerField)).getText().toString();
+                Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                data.putExtra("string1", storeQuestion); // puts one string into the Intent, with the key as 'string1'
+                data.putExtra("string2", storeAnswer); // puts another string into the Intent, with the key as 'string2
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes this activity and pass data to the original activity that launched this activity
+            }
+        });
     }
 }
--- app/src/main/java/com/example/supriya/myflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/supriya/myflashcardapp/MainActivity.java
@@ -21,12 +21,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
         findViewById(R.id.my_add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivity(intent);
+                MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("string2");
+            TextView flashcard_question = findViewById(R.id.flashcard_question);
+            flashcard_question.setText(string1);
+            TextView flashcard_answer = findViewById(R.id.flashcard_answer);
+            flashcard_answer.setText(string2);
+        }
     }
 }
